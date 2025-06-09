aecddb6cdf08fbb0fccbb347867a06ad2837d042
sowmyashreeps76 <sowmyashree9976@gmail.com>
2025-06-09 18:51:42 +0530

    created addjobtitle json file on testData folder to preserve test data. written script for add job title to automate. imported login.json and addjobtitle.json files in addjobtitle spec file.

---
+++ tests/OrangeHRM/addjobtitle.spec.js
@@ -0,0 +1,33 @@
+const { test, expect } = require('@playwright/test');
+
+import data from "../OrangeHRM/testData/addjobtitle.json"
+
+import logindata from "../OrangeHRM/testData/login.json"
+
+test.describe('Add Job Title Feature', () => {
+    test('should add a new job title successfully', async ({ page }) => {
+        await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+        await page.locator("input[name='username']").fill(logindata.username)
+
+        await page.locator("input[type='password']").fill(logindata.password)
+
+        await page.locator("button[type='submit']").click()
+
+        await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
+
+
+        await page.locator('a[href="/web/index.php/admin/viewAdminModule"]').click()
+        await page.locator("//span[normalize-space(text())='Job']").click()
+        await page.locator("//a[normalize-space(text())='Job Titles']").click()
+        await page.locator('button[class="oxd-button oxd-button--medium oxd-button--secondary"]').click()
+        await page.locator("(//label[normalize-space(text())='Job Title']/following::input)[1]").fill(data.jobtitle)
+        await page.locator("//textarea[@placeholder='Type description here']").fill(data.jobDescription)
+        await page.locator("//textarea[@placeholder='Add note']").fill(data.Note)
+        await page.locator('button[type="submit"]').click()
+        await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewJobTitleList')
+        await expect(page.locator("//p[text()='Invalid credentials']")).toBeVisible()
+
+
+    });
+});

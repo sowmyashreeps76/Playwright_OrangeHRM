55d51ea6d0c3d674b2945c6f5497e055234bb71e
sowmyashreeps76 <sowmyashree9976@gmail.com>
2025-06-10 12:01:35 +0530

    Created Admin folder and moved addjobtitle spec file to Admin

--- tests/OrangeHRM/Admin/addjobtitle.spec.js
+++ tests/OrangeHRM/Admin/addjobtitle.spec.js
@@ -1,11 +1,13 @@
 const { test, expect } = require('@playwright/test');
 
-import data from "../OrangeHRM/testData/addjobtitle.json"
+import data from "../testData/addjobtitle.json"
 
-import logindata from "../OrangeHRM/testData/login.json"
+import logindata from "../testData/login.json"
 
 test.describe('Add Job Title Feature', () => {
+
     test('should add a new job title successfully', async ({ page }) => {
+
         await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
         await page.locator("input[name='username']").fill(logindata.username)
@@ -26,7 +28,9 @@ test.describe('Add Job Title Feature', () => {
         await page.locator("//textarea[@placeholder='Add note']").fill(data.Note)
         await page.locator('button[type="submit"]').click()
         await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewJobTitleList')
-        
+
+        await page.close()
+
 
 
     });

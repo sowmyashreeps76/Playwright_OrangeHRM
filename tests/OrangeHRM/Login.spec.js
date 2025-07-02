d546cd1af3342ea3fb1f59679e2a464bcb8d09df
sowmyashreeps76 <sowmyashree9976@gmail.com>
2025-07-02 22:57:20 +0530

    Grouping the tsts

--- tests/OrangeHRM/Login.spec.js
+++ tests/OrangeHRM/Login.spec.js
@@ -1,6 +1,11 @@
 import { test, expect } from '@playwright/test';
 import logindata from "../OrangeHRM/testData/login.json"
 
+
+
+test.describe("Verify login functionality", async() => {  // Grouping the test cases
+
+
 test("Verify login with valid credentials", async ({page})=>{
 
     const creds = {
@@ -18,9 +23,7 @@ await page.locator("button[type='submit']").click()
 
 await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
 
-}
-
-)
+})
 
 test("Verify login with Invalid username and valid password", async ({page})=>{
 
@@ -35,8 +38,8 @@ await page.locator("button[type='submit']").click()
 await expect(page.locator("p.oxd-text.oxd-text--p.oxd-alert-content-text")).toBeVisible()
 
 }
-
 )
+
 test("Verify login with valid username and Invalid password", async ({page})=>{
 
 await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
@@ -68,3 +71,4 @@ await expect(page.locator("p.oxd-text.oxd-text--p.oxd-alert-content-text")).toBe
 }
 
 )
+})
---
+++ tests/groupingTests.spec.js
@@ -0,0 +1,40 @@
+import { test, expect } from '@playwright/test';
+
+
+//Grouping the test cases
+//multiple group of tests is possible
+
+test.describe("Group1", () => {
+    // test.only("test1", () => {
+    // test.skip("test1", () => {
+        test.fail("test1", () => {
+        console.log("test1");
+
+    })
+    test("test2", () => {
+        console.log("test2");
+
+    })
+})
+
+test.describe("Group2", () => {
+    test.fixme("test3", () => {
+        console.log("test3");
+
+    })
+    test("test4", () => {
+        console.log("test4");
+
+    })
+})
+
+
+// Annotations
+
+//  .only - Run specific test
+//  .skip - skip the test
+//  .fail()- intentionally failing the test
+//  .slow()- tripple the test time out for slowly running test
+//  .fixme ()- for maintanance - it will not run test cases when remove the fixme then only run like .skip
+
+
